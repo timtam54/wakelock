6f17a7336a3dfbf5b7d1fd57ed3e4c3505bf9ec3
timhams <timtam54@hotmail.com>
2025-02-28 22:47:24 +1000

    add sw

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+    <head>
+      <link rel="manifest" href="/manifest.json" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
