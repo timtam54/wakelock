d4788542a0b2ddeefc72fd815c404c93ee439a60
timhams <timtam54@hotmail.com>
2025-03-01 22:18:56 +1000

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-      </head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
